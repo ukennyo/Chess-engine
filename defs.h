5ac10354eb1fca20d2179866e7bcc74cacbabc88
Ayden Shine <105959128+ukennyo@users.noreply.github.com>
2025-03-19 16:58:14 -0400

    updated board structre, created definitions and explained what each variable means and how it contributes to our engine

--- defs.h
+++ defs.h
@@ -1,6 +1,8 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+// unsigned 64-bit integer; can hold a large non-negative integer value (from 0 upto 615)
+// used in a chess engine to efficiently represent the positions of pieces, especially the pawns
 typedef unsigned long long U64;
 
 #define NAME "Vice 1.0"
@@ -25,5 +27,42 @@ enum {
     A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ
 };
 enum {TRUE, FALSE};
+
+//Board Structure definitions
+typedef struct {
+    int piece[BRD_SQ_NUM];
+    //array named pawns, holds upto 3 'U64' values, which represnet a specific set of pawns
+    //pawns[0] represent all white, pawns [1] represent all black, pawns[2] represent all pawns white and black on the board
+    U64 pawns[3];
+    //KingSq holds two values to store the square number occupied by the black and white king
+    int KingSq[2];
+    //side shows the current side to move
+    int side;
+    // the en passant square
+    //En passant is a special pawn capture move in chess that can occur immediately after a pawn moves two squares forward from its starting position
+    //if the en passant move is used, then enPas is set to NO_SQ
+    int enPas;
+    //fifty move counter to detect a draw
+    int fiftyMove;
+    //half moves, in order for our chess engine to figure out what move is the best course of action, our engine searches through a certain number of 'ply' vlaues ahead and evaluates the best potential move.
+    //ply value helps manage various move orders and track the progress of the search within a specific variation
+    int ply;
+    //total number of half-moves made in the game so far, used for detecting threefold repetition(detecting draw, when exact same position appears on the boeard three times during the course of a game)
+    int histPly;
+    // unique key generated for each position ( U64 format makes the key more unique)
+    U64 posKey;
+    // stores the number of pieces of each type on the board
+    // for example: pceNum[2] would give the number of wite knights on the board, as our second piece listed in our piece list is wN, for white knights on the board
+    int pceNum[13];
+    //stores the number of non-pawns on the board (black, white and both)
+    int bigPce[3];
+    //stores the number of rooks and queens for black, white and both
+    int majPce[3];
+    //stores the number of knights and bishops for black, white and both
+    //used in position evaluation, especially during for draw endgames
+    //helps determine it a game can be draw based on material combinations
+    int minPce[3];
+
+} S_BOARD;
 
 #endif //DEFS_H
